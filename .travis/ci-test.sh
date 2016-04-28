8ce1b64bcdd951ebe08842090f9526c59bbca426
eXtremeSHOK.com <admin@extremeshok.com>
2016-04-28 21:55:32 +0200

    enable travis testing

--- .travis/ci-test.sh
+++ .travis/ci-test.sh
@@ -3,19 +3,19 @@
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/local/musl/bin:$HOME/bin
 
 #TEMP exit with OK
-echo .. OK
-exit 0
+# echo .. OK
+# exit 0
 
-
-if bash powerdns-to-gdnsd.sh ; then
+#run the script
+if bash powerdns-to-gdnsd.sh --output /tmp/export ; then
 	echo .. OK
 else
  	echo .. ERROR
   exit 1
 fi
 
-#check cron file generation
-if [ -e "/export/sampledomain.com" ] ; then
+#check zonefile generation
+if [ -e "/tmp/export/example.com" ] ; then
 	echo .. OK	
 else
 	echo .. ERROR
