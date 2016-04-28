ac8e7832976575d792d8aab68c4107276fb6b46f
eXtremeSHOK.com <admin@extremeshok.com>
2016-04-28 18:57:03 +0200

    travis checks for db import

--- .travis/ci-setup.sh
+++ .travis/ci-setup.sh
@@ -7,10 +7,40 @@ ORIG_PWD=$PWD
 
 echo "Creating the PowerDNS Database and User"
 mysql --host=localhost --user=root  << EOF
-CREATE DATABASE powerdns character set utf8;
+CREATE DATABASE IF NOT EXISTS powerdns character set utf8;
 GRANT ALL ON powerdns.* TO 'powerdns'@'%' IDENTIFIED BY '8wksjehkaj';
 FLUSH PRIVILEGES;
 EOF
+if [ "$?" -eq 0 ] ; then
+    echo "Success"
+else
+    echo "Error"
+    exit 1
+fi
 
 echo "Importing the PowerDNS mysql schema"
 mysql --host=localhost --user=root --database=powerdns < ".travis/tests/powerdns/schema.mysql.sql"
+if [ "$?" -eq 0 ] ; then
+    echo "Success"
+else
+    echo "Error"
+    exit 1
+fi
+
+echo "Importing the PowerDNS mysql example.com zone"
+mysql --host=localhost --user=root --database=powerdns < ".travis/tests/powerdns/mysql-zones/example.com.sql"
+if [ "$?" -eq 0 ] ; then
+    echo "Success"
+else
+    echo "Error"
+    exit 1
+fi
+
+echo "Testing database is correctly configured"
+myresult=$(mysql --user=powerdns --password="8wksjehkaj" --database=powerdns --skip-column-name -e "SELECT name FROM domains WHERE name='example.com' ")
+if [ "$myresult" == "example.com" ] ; then
+  echo "Success"
+else
+  echo "Error"
+  exit 1
+fi
