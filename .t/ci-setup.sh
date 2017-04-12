877673e96af8687ab0a9ac7734ffcbe64dd17e94
eXtremeSHOK <admin@extremeshok.com>
2017-04-12 12:23:31 +0200

    path fix

--- .t/ci-setup.sh
+++ .t/ci-setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/local/musl/bin:$HOME/bin
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/local/musl/bin:/home/travis
 
 #save the current dir for later
 #ORIG_PWD=$PWD
 
 echo "Creating the PowerDNS Database and User"
-mysql --host=localhost --user=root  << EOF
+mysql --host=localhost --user=root << EOF
 CREATE DATABASE IF NOT EXISTS powerdns character set utf8;
 GRANT ALL ON powerdns.* TO 'powerdns'@'%' IDENTIFIED BY '8wksjehkaj';
 FLUSH PRIVILEGES;
